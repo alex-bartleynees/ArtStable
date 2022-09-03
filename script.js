713a273186dec4ca75588d0288421df7900e90d9
alex-bartleynees <alexbartleynees@gmail.com>
2022-09-03 12:07:58 +1200

    Fix bug

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const sliders = function () {
 
   const createDots = function () {
     slides.forEach((_, i) => {
-      dotContainer.insertAdjacentHTML(
+      dotContainer?.insertAdjacentHTML(
         "beforeend",
         `<button class="dots__dot" data-slide="${i}"></button>`
       );
